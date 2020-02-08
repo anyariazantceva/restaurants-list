73dd667626257d5d9c753566e0a0f9e79497ad6f
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-08 22:57:25 +0200

    Created sorting functions

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let listContainer = document.querySelector('.page__restaurants');
+let ascBtn = document.querySelector('.btn__asc');
+let descBtn = document.querySelector('.btn__desc');
 fetch('restaurants.json')
     .then(res => res.json())
     .then((data) => {
         const restaurants = data.restaurants;
-        restaurants.forEach((item) => {
-            let block = createItem(item);
-            listContainer.append(block);
+        renderList(restaurants);
+        ascBtn.addEventListener('click', () => {
+            let sortedArr = sortAsc(restaurants);
+            clearContainer();
+            renderList(sortedArr);
+        })
+        descBtn.addEventListener('click', () => {
+            let sortedArr = sortDesc(restaurants);
+            clearContainer();
+            renderList(sortedArr);
         })
     });
 
@@ -26,3 +35,33 @@ const createElement = (el, className) => {
     element.classList.add(className);
     return element
 };
+
+const renderList = (arr) => {
+    arr.forEach((item) => {
+        let block = createItem(item);
+        listContainer.append(block);
+    })
+};
+
+const clearContainer = () => {
+    let listContainer = document.querySelector('.page__restaurants');
+    listContainer.innerHTML = '';
+};
+
+const sortAsc = (arr) => {
+    let sortedArr = arr.sort(function(a, b){
+        if(a.name < b.name) {
+            return -1;
+        }
+    });
+    return sortedArr
+};
+
+const sortDesc = (arr) => {
+    let sortedArr = arr.sort(function(a, b) {
+        if(a.name > b.name) {
+            return -1;
+        }
+    })
+    return sortedArr;
+}
